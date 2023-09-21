95a7fea8fdc1f9e606e68bceb2acc4c567657910
22joka04 <22joka04@HW-RDSSH-02.HIG-AD.hig.se>
2023-09-21 14:52:57 +0200

    Create Panels fianl step

--- src/view/MainPanel.java
+++ src/view/MainPanel.java
@@ -6,7 +6,19 @@ import javax.swing.JPanel;
 
 public class MainPanel extends JPanel {
 	
+	private JPanel clockPanel = new ClockPanel();
+	private JPanel buttonPanel = new ButtonPanel();
+	private JPanel consolkPanel = new ConsolPanel();
+	private JPanel textBoxPanel = new TextBoxPanel();
+	
+	
 	public MainPanel() {
 		setLayout(new GridLayout(2,2));
+		
+		this.add(clockPanel);
+		this.add(buttonPanel);
+		this.add(consolkPanel);
+		this.add(textBoxPanel);
+	
 	}
 }
