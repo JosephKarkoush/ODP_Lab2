898c85d98f40a1fe5928c610613f6f4702a7718c
22joka04 <22joka04@hw-rdssh-04.HIG-AD.hig.se>
2023-09-22 15:43:56 +0200

    sdsd

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -28,6 +28,10 @@ public class Controller {
 		timer.start();
 
 	}
+	
+	public String getTimeController() {
+		return weekAlarmclock.getTime().toString();
+	}
 
 	public void settimeWitController() {
 
